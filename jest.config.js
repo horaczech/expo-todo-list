6f5ffdf21b96d208b748e7b7ea24aff9602444b4
Petr Horacek <p3trh0r4c3k@gmail.com>
2023-11-04 08:20:00 +0100

    config jest for testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-expo',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+};
